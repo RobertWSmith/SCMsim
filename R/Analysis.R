0d37f34006404d308a72edf38196721173d465d1
Robert Smith <robert.w.smith08@gmail.com>
2013-05-30 09:48:46 -0400

    updates to save.jpeg, save.pdf
    set jpeg resolution to 1920 x 1080, made save.pdf a function which
    automates the list of ggplot graphs I create (and is flexible enough to
    accept changes for both pdf and jpeg)

--- R/Analysis.R
+++ R/Analysis.R
@@ -6,21 +6,21 @@ save.pdf <- function(l.plots) {
     temp.list <- l.plots[[i]]
     pdf(paste0("C:/Users/a421356/R-GitHub/SCMsim/Output/Graphics/", f.name[i], ".pdf"), 
         width = 11, height = 8.5, onefile = TRUE, title = f.name[i])
-    for (j in 1:temp.list) {
+    for (j in 1:length(temp.list)) {
       print(temp.list[[j]])
     }
     dev.off()
   }
 }
 
-save.png <- function(l.plots) {
+save.jpeg <- function(l.plots) {
   f.name <- names(l.plots) 
   for (i in 1:length(l.plots)) {
     temp.list <- l.plots[[i]]
     p.names <- names(temp.list)
     for (j in 1:length(temp.list)) {
-      png(paste0("C:/Users/a421356/R-GitHub/SCMsim/Output/Graphics/", f.name[i], " ", p.names, 
-                 ".png"), width = 11, height = 8.5, onefile = TRUE, title = f.name[i])
+      jpeg(paste0("C:/Users/a421356/R-GitHub/SCMsim/Output/Graphics/", f.name[i], " ", p.names, 
+                 ".jpg"), width = 1920, height = 1080, units = "px")
       print(temp.list[[j]])
       dev.off()
     }
@@ -47,42 +47,51 @@ for (i in 1:length(f.name)) {
   whole.sim <- data[(data$factory == f.name[i]), 1:8]
   disrupt <- whole.sim[200:400, ]
   
-  temp.list <- vector("list", length = 5)
-  names(temp.list) <- c("daily.inv", "in.transit.inv", "disruption", "fcst.err", "act.err")
+  tw <- ggplot(whole.sim)
+  td <- ggplot(disrupt)
   
-  temp.list[[1]] <- dat.ss + stat_smooth(mapping = aes(x = date, y = daily_inv), 
+  temp.list <- vector("list", length = 6)
+  names(temp.list) <- c("daily_inv", "in_transit_inv", "disruption", "fcst_err", "act_err", "err_density")
+  
+  temp.list[[1]] <- tw + stat_smooth(mapping = aes(x = date, y = daily_inv), 
                                          geom = "line", col = "red") + 
     geom_point(aes(x = date, y = daily_inv)) + 
     labs(list(title = paste("Daily Inventory -", f.name[i]), 
               x = "Simulation Days", y = "Daily Inventory (kgs.)" ))
   
-  temp.list[[2]] <- dat.ss + stat_smooth(mapping = aes(x = date, y = in_transit), 
+  temp.list[[2]] <- tw + stat_smooth(mapping = aes(x = date, y = in_transit), 
                                          geom = "line", col = "red") + 
     geom_point(aes(x = date, y = in_transit)) + 
     labs(list(title = paste("In Transit Inventory -", f.name[i]), 
               x = "Simulation Days", y = "In Transit Inventory (kgs.)" ))
   
-  temp.list[[3]] <- dat.ss + stat_smooth(mapping = aes(x = date, y = forecast_err), 
+  temp.list[[3]] <- td + stat_smooth(mapping = aes(x = date, y = daily_inv), 
+                                     geom = "line", col = "red") + 
+    geom_point(aes(x = date, y = daily_inv)) + 
+    labs(list(title = paste("Disruption Daily Inventory -", f.name[i]), 
+              x = "Simulation Days", y = "Daily Inventory (kgs.)" ))
+  
+  temp.list[[4]] <- tw + stat_smooth(mapping = aes(x = date, y = forecast_err), 
                                          geom = "line", col = "red") + 
     geom_point(aes(x = date, y = forecast_err)) + 
     labs(list(title = paste("Forecast Error -", f.name[i]), 
               x = "Simulation Days", y = "Forecast Error (kgs.)" ))
   
-  temp.list[[4]] <- dat.ss + geom_point(aes(x = date, y = forecast_err), col = "red") + 
+  temp.list[[5]] <- tw + geom_point(aes(x = date, y = forecast_err), col = "red") + 
     labs(list(title = paste("Daily Inventory & Forecast Error -", f.name[i]), 
               x = "Simulation Days", y = "Material (kgs.)" )) + 
     geom_point(aes(x = date, y = daily_inv), col = "black") +
     stat_smooth(mapping = aes(x = date, y = daily_inv), geom = "line", col = "red") + 
     stat_smooth(mapping = aes(x = date, y = forecast_err), geom = "line", col = "black")
   
-  temp.list[[5]] <- dat.ss + geom_histogram(mapping = aes(x = forecast_err, y = ..density..)) + 
+  temp.list[[6]] <- tw + geom_histogram(mapping = aes(x = forecast_err, y = ..density..)) + 
     geom_density(aes(x = forecast_err, y = ..density..), col = "red")
   
   plots[[i]] <- temp.list
 }
 
 save.pdf(plots)
-save.png(plots)
+# save.jpeg(plots)
 
 
 
